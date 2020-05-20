b531dbf919ce5caf0b8fd68dc8133ef1edd7e413
José Eduardo Méndez Farnes <twokadoz@galileo.edu>
2020-05-20 12:15:32 -0600

    ViewModels on the Engine scope created
    Created an instance of Player with the current name/gold, the object is created and the next step is to retrieve this information to make it visible to the user in the graphic interface

---
+++ Engine/Models/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine.Models
+{
+    public class Player{
+        public String Name { get; set; }
+        public String CharacterClass { get; set; }
+        public int HitPoints { get; set; }
+        public int ExperienceLevel { get; set; }
+        public int Level { get; set; }
+        public int Gold { get; set; }
+    }
+}
---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Engine.Models;
+
+namespace Engine.ViewModels
+{
+    class GameSession{
+        Player CurrentPlayer { set; get; }
+
+        public GameSession(){
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Rangus";
+            CurrentPlayer.Gold = 100000;
+        }
+    }
+}
