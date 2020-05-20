eb50456bb81d1fb593512519ff264d650eb4871a
José Eduardo Méndez Farnes <twokadoz@galileo.edu>
2020-05-20 15:03:05 -0600

    All player data is being displayed on the GUI
    Retrieveed successfully all data that displays the current status of a player, working on the button to add more XP
    
    Clicking on the butto does not add the 10 more XP to the user just yet.

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -5,13 +5,17 @@ using Engine.Models;
 
 namespace Engine.ViewModels
 {
-    class GameSession{
-        Player CurrentPlayer { set; get; }
+    public class GameSession{
+        public Player CurrentPlayer { set; get; }
 
         public GameSession(){
             CurrentPlayer = new Player();
             CurrentPlayer.Name = "Rangus";
             CurrentPlayer.Gold = 100000;
+            CurrentPlayer.CharacterClass = "Hunter";
+            CurrentPlayer.HitPoints = 250;
+            CurrentPlayer.ExperienceLevel = 0;
+            CurrentPlayer.Level = 1;
         }
     }
 }
--- RougeRPG/MainWindow.xaml.cs
+++ RougeRPG/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Engine.ViewModels;
 
 namespace RougeRPG
 {
@@ -20,9 +21,17 @@ namespace RougeRPG
     /// </summary>
     public partial class MainWindow : Window
     {
+        private GameSession _gameSession;
         public MainWindow()
         {
             InitializeComponent();
+            _gameSession = new GameSession();
+            DataContext = _gameSession;
+        }
+
+        private void ButtonBase_OnClick(Object sender, RoutedEventArgs e)
+        {
+            _gameSession.CurrentPlayer.ExperienceLevel += 10;
         }
     }
 }
